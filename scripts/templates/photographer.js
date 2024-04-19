e1552fc720a7e4ebceac676f0b871b682bb04dc5
Frédérique <tarroux.frederique@gmail.com>
2024-04-19 16:19:54 +0200

    Envoie du projet avec la page d'accueil presque finis

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/PhotographersIDPhotos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        // article.innerHTML = (
+        //     <img src={picture}/>
+        // )
+        const imgContainer = document.createElement('div');
+        imgContainer.classList.add('card-photographer');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
+        const locationDiv = document.createElement('div');
+        locationDiv.classList.add('location');
+        locationDiv.textContent = city + ", " + country;
+        const taglineEl = document.createElement('p');
+        taglineEl.classList.add('taglineElement');
+        taglineEl.textContent = tagline;
+        const priceEl = document.createElement('p');
+        priceEl.classList.add('priceElement');
+        priceEl.textContent = price + "/jour";
+        imgContainer.appendChild(img);
+        article.appendChild(imgContainer);
         article.appendChild(h2);
+        article.appendChild(locationDiv);
+        article.appendChild(taglineEl);
+        article.appendChild(priceEl);
         return (article);
     }
     return { name, picture, getUserCardDOM }
